4591a44777a9d439297f4886d1e6101b464c4e4a
tanzilahmed0 <tanahmed03@gmail.com>
2025-09-16 22:11:06 -0700

    Bug Fixes

--- Homework 2/hw2_SJSUID/question_1/library.py
+++ Homework 2/hw2_SJSUID/question_1/library.py
@@ -1,6 +1,10 @@
+from utils import log_operation, filter_by_author, book_generator
+
+@log_operation
 def add_book(library, title, author): 
     library.append({'title': title, 'author': author})
 
+@log_operation
 def remove_book(library, title):
     found = False
     for book in library:
