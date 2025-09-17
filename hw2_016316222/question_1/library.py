bb6e0eeb9a54b22a391dd32efa9542658790bc17
tanzilahmed0 <tanahmed03@gmail.com>
2025-09-16 22:37:07 -0700

    Fixes

---
+++ hw2_016316222/question_1/library.py
@@ -0,0 +1,23 @@
+from .utils import log_operation, filter_by_author, book_generator
+
+@log_operation
+def add_book(library, title, author): 
+    library.append({'title': title, 'author': author})
+    print(f"Added book: {title} by {author}")
+
+@log_operation
+def remove_book(library, title):
+    for i, book in enumerate(library):
+        if book['title'] == title: 
+            library.pop(i)
+            return
+    print(f"Error: Book '{title}' not found.")
+
+def list_books(library):
+    if not library:
+        print("The library is empty.") 
+    else: 
+        for book in library: 
+            print(f"{book['title']} by {book['author']}")
+
+
