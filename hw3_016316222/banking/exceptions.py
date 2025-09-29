9baf560a5e80c24bd7a1e9e9cc145731ddb71847
tanzilahmed0 <tanahmed03@gmail.com>
2025-09-28 20:38:52 -0700

    Implemented Exception Classes

---
+++ hw3_016316222/banking/exceptions.py
@@ -0,0 +1,13 @@
+class InsufficientFundsError(Exception):
+    def __init__(self, message, error_code, balance=None):
+        super().__init__(message) 
+        self.message = message
+        self.error_code = error_code
+        self.balance = balance
+
+class InvalidAmountError(Exception):
+    def __init__(self, message, error_code, amount = None): 
+        suepr().__init__(message)
+        self.message = message 
+        self.error_code = error_code
+        self.amount = amount
