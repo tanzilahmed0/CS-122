3a7c32eaa1bc07afd2c716f901a60ecff7a9c0fd
tanzilahmed0 <tanahmed03@gmail.com>
2025-10-01 17:48:04 -0700

    Decorator

--- practice_exam_solution/main.py
+++ practice_exam_solution/main.py
@@ -1,1 +1,36 @@
 # Your solutions for Problems 1 and 2 go here.
+import 
+# Your solutions for Problems 1 and 2 go here.
+class CourseFullError(Exception): 
+    pass 
+
+class Student: 
+    def __init__(self, student_id, name): 
+        if not is_valid_student_id(student_id): 
+            raise ValueError("Not Valid Student ID")
+
+        self.student_id = student_id 
+        self.name = name 
+
+class Course: 
+    def __init__(self, course_name, capacity): 
+        self.students = [] 
+        self.course_name = course_name 
+        self.capacity = capacity 
+
+    def add_student(self, student): 
+        if len(self.students) + 1 > self.capacity: 
+            raise CourseFullError("Course is full")
+        
+        else: 
+            self.students.append(student)
+
+
+def log_call(func): 
+    def wrapper(*args, **kwargs): 
+        print(f"[LOG] Calling function {func}")
+
+        func(*args, **kwargs) 
+
+    return wrapper
+
