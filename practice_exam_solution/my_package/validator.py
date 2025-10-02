3e40a373dd558293bad5ce0406660dcf7d0cf0ad
tanzilahmed0 <tanahmed03@gmail.com>
2025-10-01 18:19:03 -0700

    Regex

--- practice_exam_solution/my_package/validator.py
+++ practice_exam_solution/my_package/validator.py
@@ -1,2 +1,9 @@
 # Your solution for Problem 3 goes here.
+import re 
 def is_valid_student_id(student_id): 
+    pattern = r"^S\d{5}$"
+
+    if not re.search(pattern, student_id): 
+        return False
+    else: 
+        return True
